8699f17992a221db5ea7d0c8469e331630672912
Piksie <jaka.seliskar@gmail.com>
2022-01-28 18:00:05 +0100

    Article section layout

--- main.js
+++ main.js
@@ -3,12 +3,13 @@
 // document.body for Chrome, Safari, Opera
 // document.documentElement for IE and Mozilla
 
-let header = document.getElementById("header-container");
+let headerScroll = document.getElementById("header-container");
+let headerNavBtn = document.getElementById("a.nav-button");
 
 window.onscroll = () => {
     if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
-        header.classList.add("header-scroll")
+        headerScroll.classList.add("header-scroll")
     } else {
-        header.classList.remove("header-scroll")
+        headerScroll.classList.remove("header-scroll")
     }
 };  

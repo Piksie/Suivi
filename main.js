268d98a753407079efe0b0b855f110975b353ea2
Piksie <jaka.seliskar@gmail.com>
2022-01-30 11:36:36 +0100

    Basic footer layout

--- main.js
+++ main.js
@@ -6,10 +6,12 @@
 let headerScroll = document.getElementById("header-container");
 let headerNavBtn = document.getElementById("a.nav-button");
 
+
+
 window.onscroll = () => {
     if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
-        headerScroll.classList.add("header-scroll")
+        headerScroll.classList.add("header-scroll");
     } else {
-        headerScroll.classList.remove("header-scroll")
+        headerScroll.classList.remove("header-scroll");
     }
 };  
